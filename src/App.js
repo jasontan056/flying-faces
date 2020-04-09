e857e059fe589cbfab1a41d62ddb7194fbef328d
jasontan056 <jasontan056@gmail.com>
2020-04-09 15:56:26 -0700

    Add editor page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import RecordVideo from "./RecordVideo";
+import Editor from "./Editor";
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/editor">
+          <Editor />
+        </Route>
         <Route path="/">
           <RecordVideo />
         </Route>
---
+++ src/Editor.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Editor(props) {
+  return <div>editor</div>;
+}
+
+Editor.propTypes = {};
+
+export default Editor;
