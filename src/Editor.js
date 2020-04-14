c6bda4a8d3752a9999d7a8603843160994d2b957
jasontan056 <jasontan056@gmail.com>
2020-04-13 17:57:02 -0700

    Save any mask that was drawn on a frame.

--- src/Editor.js
+++ src/Editor.js
@@ -17,10 +17,39 @@ function Editor({ frames = [] }) {
 
   const p5Ref = useRef(null);
   const sliderValueRef = useRef(0);
+  const masks = useRef({});
 
   useEffect(() => {
     const sketch = (p5) => {
       let maskLayer;
+      let lastFrameIdx;
+
+      const copyImage = (src) => {
+        const copy = p5.createImage(src.width, src.height);
+        copy.copy(
+          src,
+          0,
+          0,
+          src.width,
+          src.height,
+          0,
+          0,
+          src.width,
+          src.height
+        );
+
+        return copy;
+      };
+
+      const mouseIsPressedAndInCanvas = () => {
+        return (
+          p5.mouseIsPressed &&
+          p5.mouseX >= 0 &&
+          p5.mouseX < p5.width &&
+          p5.mouseY >= 0 &&
+          p5.mouseY < p5.height
+        );
+      };
 
       p5.setup = () => {
         p5.createCanvas(640, 480);
@@ -30,52 +59,40 @@ function Editor({ frames = [] }) {
 
       p5.draw = () => {
         const frameIdx = sliderValueRef.current;
+
+        if (frameIdx !== lastFrameIdx) {
+          maskLayer.clear();
+          const existingMask = masks.current[frameIdx];
+          if (existingMask) {
+            maskLayer.image(existingMask, 0, 0);
+          }
+
+          lastFrameIdx = frameIdx;
+        }
+
         const frame = frames[frameIdx];
         p5.image(frame, 0, 0);
 
-        if (p5.mouseIsPressed) {
+        if (mouseIsPressedAndInCanvas()) {
           maskLayer.fill(255, 0, 0, 255);
           maskLayer.noStroke();
           maskLayer.ellipse(p5.mouseX, p5.mouseY, 30);
+          masks.current[lastFrameIdx] = copyImage(maskLayer);
         }
 
-        // Create a copy of the mask layer to draw the brushed area on the canvas.
-        const paintGuide = p5.createImage(maskLayer.width, maskLayer.height);
-        paintGuide.copy(
-          maskLayer,
-          0,
-          0,
-          maskLayer.width,
-          maskLayer.height,
-          0,
-          0,
-          maskLayer.width,
-          maskLayer.height
-        );
-        paintGuide.loadPixels();
-        for (let i = 0; i < paintGuide.pixels.length; i += 4) {
-          const alpha = paintGuide.pixels[i + 3];
-          if (alpha > 0) {
-            paintGuide.pixels[i + 3] = 50;
+        if (frameIdx in masks.current) {
+          // Create a copy of the mask layer to draw the brushed area on the canvas.
+          const paintGuide = copyImage(maskLayer);
+          paintGuide.loadPixels();
+          for (let i = 0; i < paintGuide.pixels.length; i += 4) {
+            const alpha = paintGuide.pixels[i + 3];
+            if (alpha > 0) {
+              paintGuide.pixels[i + 3] = 50;
+            }
           }
+          paintGuide.updatePixels();
+          p5.image(paintGuide, 0, 0);
         }
-        paintGuide.updatePixels();
-        p5.image(paintGuide, 0, 0);
-
-        // Build masked frame.
-        const maskedFrame = p5.createImage(frame.width, frame.height);
-        maskedFrame.copy(
-          frame,
-          0,
-          0,
-          frame.width,
-          frame.height,
-          0,
-          0,
-          frame.width,
-          frame.height
-        );
-        maskedFrame.mask(maskLayer);
       };
     };
 
@@ -85,7 +102,6 @@ function Editor({ frames = [] }) {
   const valueText = useCallback((value) => `Frame ${value}`, []);
 
   const handleSliderChange = useCallback((event, newValue) => {
-    console.log(newValue);
     sliderValueRef.current = newValue;
   }, []);
 
