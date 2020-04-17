a05856e9122a83ef5cc2e0b7aacab42cde72978e
jasontan056 <jasontan056@gmail.com>
2020-04-16 22:06:55 -0700

    Export videos

--- src/Player.js
+++ src/Player.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useCallback } from "react";
 import p5 from "p5";
 import { makeStyles } from "@material-ui/core/styles";
+const createCanvasRecorder = require("canvas-record");
 
 const useStyles = makeStyles({
   root: {
@@ -11,23 +12,37 @@ const useStyles = makeStyles({
   },
 });
 
+const CANVAS_ID = "PLAYER_CANVAS";
+
 function Player({ frames = [] }) {
   const classes = useStyles();
 
   const p5Ref = useRef(null);
-  const playing = useRef(true);
-  const curIdx = useRef(0);
+  const playingRef = useRef(true);
+  const curIdxRef = useRef(0);
+  const recorderRef = useRef(null);
 
   useEffect(() => {
     const sketch = (p5) => {
       p5.setup = () => {
-        p5.createCanvas(640, 480);
+        const canvas = p5.createCanvas(640, 480);
+        canvas.id(CANVAS_ID);
       };
 
       p5.draw = () => {
-        if (playing.current && curIdx.current < frames.length) {
-          p5.image(frames[curIdx.current], 0, 0);
-          curIdx.current++;
+        if (!playingRef.current) {
+          return;
+        }
+        if (curIdxRef.current < frames.length) {
+          p5.image(frames[curIdxRef.current], 0, 0);
+          curIdxRef.current++;
+        } else {
+          playingRef.current = false;
+          if (recorderRef.current) {
+            recorderRef.current.stop();
+            recorderRef.current.dispose();
+            recorderRef.current = null;
+          }
         }
       };
     };
@@ -38,18 +53,28 @@ function Player({ frames = [] }) {
   }, [frames]);
 
   const onPlayButtonClicked = () => {
-    playing.current = true;
+    playingRef.current = true;
   };
 
   const onStopButtonClicked = () => {
-    playing.current = false;
-    curIdx.current = 0;
+    playingRef.current = false;
+    curIdxRef.current = 0;
+  };
+
+  const onExportButtonClicked = () => {
+    curIdxRef.current = 0;
+    recorderRef.current = createCanvasRecorder(
+      document.getElementById(CANVAS_ID)
+    );
+    recorderRef.current.start();
+    playingRef.current = true;
   };
 
   return (
     <div className={classes.root}>
       <button onClick={onPlayButtonClicked}>Play</button>
       <button onClick={onStopButtonClicked}>Stop</button>
+      <button onClick={onExportButtonClicked}>Export</button>
       <div ref={p5Ref}></div>
     </div>
   );
